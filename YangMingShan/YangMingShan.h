579ff5697ef40c91b6b28205557b81089ec58e74
Yoshitaka Kazue <ness.kazuyoshi@gmail.com>
2016-11-16 22:41:10 +0900

    Added Carthage support

---
+++ YangMingShan/YangMingShan.h
@@ -0,0 +1,19 @@
+//
+//  YangMingShan.h
+//  YangMingShan
+//
+//  Created by NESS on 2016/11/16.
+//  Copyright © 2016年 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YangMingShan.
+FOUNDATION_EXPORT double YangMingShanVersionNumber;
+
+//! Project version string for YangMingShan.
+FOUNDATION_EXPORT const unsigned char YangMingShanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YangMingShan/PublicHeader.h>
+
+
